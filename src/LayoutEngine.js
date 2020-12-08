04ff04b7c38838266652d0746dfac3bbf6cbeb10
Asha Reddy <ashabnhere@gmail.com>
2020-12-08 12:24:41 +0000

    move the reportPageData to a different file

--- src/LayoutEngine.js
+++ src/LayoutEngine.js
@@ -1,25 +1,4 @@
-var reportPageData = { 
-	id: "guid",
-	type: "REPORT",
-	version: 1,
-	children: [{
-		id: "guid",
-		type: "SLIDE",
-		size: "A4",
-		orientation: "portrait",
-		children: [{
-			type: "TEXT",
-			widgetId: 1,
-			left: 0,
-        top: 0,
-        width: 0,
-        height: 0
-		}, {
-			type: "CHART",
-			widgetId: 2
-		}]
-	}]
-};
+import { reportPageData } from './reportPageData';
 
 function LayoutEngine() {
   return (
---
+++ src/reportPageData.js
@@ -0,0 +1,22 @@
+export const reportPageData = { 
+	id: "guid",
+	type: "REPORT",
+	version: 1,
+	children: [{
+		id: "guid",
+		type: "SLIDE",
+		size: "A4",
+		orientation: "portrait",
+		children: [{
+			type: "TEXT",
+			widgetId: 1,
+			left: 0,
+        top: 0,
+        width: 0,
+        height: 0
+		}, {
+			type: "CHART",
+			widgetId: 2
+		}]
+	}]
+};
